2dd058594915f725832d7efbd2d1aa2c9b14d6b5
Timur Tuktarov <tima.xxxyyy@mail.ru>
2025-10-23 21:29:46 +0300

    Pr3 'done' in verilog

--- Python_misc/translator.py
+++ Python_misc/translator.py
@@ -1,17 +1,38 @@
 import re
 
 # Словарь кодов операций
+# localparam NOP = 0, LTM = 1, MTR = 2, RTR = 3, JL = 4, SUB = 5, SUM = 6, MTRK = 7, RTM = 8, JMP = 9;
+
+ma = 5 # memory address
+lt = 10 # literal
+ra = 4 # register address
+pc = 4 # prog counter
+
 opcodes = {
-    'LTM': 0b0000,
-    'RTM': 0b0001,
-    'RTR': 0b0010,
-    'JL': 0b0011,
-    'SUB': 0b0100,
-    'SUM': 0b0101,
-    'MTRK': 0b0110,
-    'JMP': 0b0111,
-    'NOP': 0b1000
+    'NOP': 0b0000,
+    'LTM': 0b0001,
+    'MTR': 0b0010,
+    'RTR': 0b0011,
+    'JL': 0b0100,
+    'SUB': 0b0101,
+    'SUM': 0b0110,
+    'MTRK': 0b0111,
+    'RTM': 0b1000,
+    'JMP': 0b1001
 }
+
+bytes = [
+    [],
+    [ma, lt],
+    [ma, ra],
+    [ra, ra],
+    [ra, ra, pc],
+    [ra, ra, ra],
+    [ra, ra, ra],
+    [ra, ra],
+    [ra, ra],
+    [pc]
+]
 
 def parse_line(line):
     """Разбирает строку и возвращает команду и аргументы"""
@@ -56,37 +77,60 @@ def to_binary(command, args):
     
     # Код операции (4 бита)
     opcode_bin = format(opcodes[command], '04b')
-    
+    # print(f"opcode: {opcode_bin}", end=" : ")
     # Аргументы (каждый по 4 бита)
     args_bin = []
-    for i in range(3):
-        if i < len(args):
-            args_bin.append(format(args[i], '04b'))
-        else:
-            args_bin.append('0000')  # Заполняем нулями отсутствующие аргументы
+
+
+    if(command == 'JL'):
+        args_bin.append(format(args[0], '04b'))
+        args_bin.append(format(args[1], '04b'))
+        args_bin.append('00')
+        args_bin.append(format(args[2], '05b'))
+    elif(command == 'JMP'):
+        args_bin.append('0' * 10)
+        args_bin.append(format(args[0], '05b'))
+    elif(command == "MTR"):
+        args_bin.append(format(args[1], '04b'))
+        args_bin.append('0')
+        args_bin.append(format(args[0], '04b'))
+    else:
+        for i in range(len(bytes[opcodes[command]])):
+            if i < len(args):
+                temp = format(args[i], f'0{bytes[opcodes[command]][i]}b')
+                # print(f"[{i}] arg: {temp}", end=" | ")
+                args_bin.append(temp)
+            else:
+                args_bin.append('0000', end=" | ")  # Заполняем нулями отсутствующие аргументы
+    
+
+            
     
     # Объединяем в 19-битную строку
     binary_string = opcode_bin + ''.join(args_bin)
     
+    binary_string = binary_string.ljust(19, '0')
     # Проверяем длину
-    if len(binary_string) != 16:
+    if len(binary_string) != 19:
         raise ValueError(f"Некорректная длина бинарной строки: {len(binary_string)}")
     
     return binary_string
 
 def main():
-    with open('commands.txt', 'r') as f:
+    with open('commands3.txt', 'r') as f:
         lines = f.readlines()
     
     for line in lines:
         result = parse_line(line)
+        # print("RESULT: ", result)
         if not result:
             continue
             
         command, args = result
         try:
             binary = to_binary(command, args)
-            print(f"{binary} // {command} {args}")
+            # print(f"{binary} // {command} {args}")
+            print(f"{binary}")
         except ValueError as e:
             print(f"Ошибка обработки строки: {line.strip()} -> {e}")
 
---
+++ Schemach/Prs/CPU_Pr3/CPU_Pr3.srcs/sources_1/new/cpu.v
@@ -0,0 +1,135 @@
+`timescale 1ns / 1ps
+
+module cpu(
+    input clk, reset,
+    output pc // что нибудь бахнуть на выход
+    );
+localparam LITERAL_SIZE = 10;
+localparam COP_SIZE = 4;
+
+localparam CMD_MEM_SIZE = 32;
+localparam CMD_ADDR_SIZE = $clog2(CMD_MEM_SIZE);
+localparam CMD_SIZE = 19;
+
+localparam MEM_SIZE = 32;
+localparam MEM_ADDR_SIZE = $clog2(MEM_SIZE);
+localparam MEM_DATA_SIZE = LITERAL_SIZE;
+
+localparam RF_SIZE = 16;
+localparam RF_ADDR_SIZE = $clog2(RF_SIZE);
+localparam RF_DATA_SIZE = LITERAL_SIZE;
+
+localparam NOP = 0, LTM = 1, MTR = 2, RTR = 3, JL = 4, SUB = 5, SUM = 6, MTRK = 7, RTM = 8, JMP = 9;
+
+reg [CMD_SIZE       -1:0] cmd_mem [0:CMD_MEM_SIZE-1];
+reg [MEM_DATA_SIZE  -1:0] mem [0:MEM_SIZE-1];
+reg [RF_DATA_SIZE   -1:0] RF [0:RF_SIZE-1];
+reg [CMD_ADDR_SIZE  -1:0] pc;
+reg [CMD_SIZE       -1:0] cmd_reg;
+reg [LITERAL_SIZE   -1:0] opA, opB;
+reg [2*LITERAL_SIZE -1:0] res;
+
+`define hi 2*LITERAL_SIZE-1 -: LITERAL_SIZE
+`define lo LITERAL_SIZE - 1: 0
+reg [2:0] stage_counter;
+
+integer i;
+initial
+begin
+    for(i = 0; i < MEM_SIZE; i = i + 1)
+        mem[i] = 0;
+    for(i = 0; i < RF_SIZE; i = i + 1)
+        RF[i] = 0;
+    RF[1] = 1;
+    
+    $readmemb("cmd_mem.mem", cmd_mem);
+end
+
+wire [COP_SIZE-1:0] cop                 = cmd_reg[CMD_SIZE-1 -: COP_SIZE];
+wire [RF_ADDR_SIZE-1:0] addr_m_1        = cmd_reg[CMD_SIZE-1 - COP_SIZE -: MEM_ADDR_SIZE];
+wire [RF_ADDR_SIZE-1:0] addr_r_1        = cmd_reg[CMD_SIZE-1 - COP_SIZE -: RF_ADDR_SIZE];
+wire [RF_ADDR_SIZE-1:0] addr_r_1_MTR    = cmd_reg[CMD_SIZE-1 - COP_SIZE - MEM_ADDR_SIZE -: RF_ADDR_SIZE];
+wire [RF_ADDR_SIZE-1:0] addr_r_2        = cmd_reg[CMD_SIZE-1 - COP_SIZE - RF_ADDR_SIZE -: RF_ADDR_SIZE];
+wire [RF_ADDR_SIZE-1:0] addr_r_3        = cmd_reg[CMD_SIZE-1 - COP_SIZE - 2*RF_ADDR_SIZE -: RF_ADDR_SIZE];
+wire [LITERAL_SIZE-1:0] literal         = cmd_reg[LITERAL_SIZE-1:0];
+wire [CMD_ADDR_SIZE-1:0] addr_to_jmp    = cmd_reg[CMD_ADDR_SIZE-1:0];
+
+always@(posedge clk)
+    if(reset || stage_counter == 4)
+        stage_counter <= 0;
+    else
+        stage_counter <= stage_counter + 1;
+    
+always@(posedge clk)
+    if(reset)
+        cmd_reg <= {(CMD_SIZE){1'b0}};
+    else
+        if(stage_counter == 0)
+            cmd_reg <= cmd_mem[pc];
+            
+always@(posedge clk)
+    if(reset)
+        opA <= {(LITERAL_SIZE){1'b0}};
+    else
+        if(stage_counter == 1)
+            case (cop)
+                LTM: opA <= addr_m_1;
+                MTR: opA <= mem[addr_m_1];
+                RTR, RTM: opA <= RF[addr_r_2];
+                JL, SUM, SUB, MTRK: opA <= RF[addr_r_1];
+            endcase
+            
+always@(posedge clk)
+    if(reset)
+        opB <= {(LITERAL_SIZE){1'b0}};
+    else
+        if(stage_counter == 2)
+            case (cop)
+                LTM: opB <= literal;
+                JL, SUM, SUB: opB <= RF[addr_r_2];
+                MTRK: opB <= mem[opA];
+                RTM: opA <= RF[addr_r_1];
+            endcase
+
+always@(posedge clk)
+    if(reset)
+        res <= {(2*LITERAL_SIZE){1'b0}};
+    else
+        if(stage_counter == 3)
+            case (cop)
+                LTM, RTM: res <= {opA, opB};
+                MTR, RTR: res <= opA;
+                MTRK: res <= opB;
+                JL: res <= opA < opB;
+                SUB: res <= opA - opB;
+                SUM: res <= opA + opB;  
+            endcase
+
+always@(posedge clk)
+    if(reset)
+        pc <= {(CMD_ADDR_SIZE){1'b0}};
+    else
+        if(stage_counter == 4)
+            case (cop)
+                JL: if(res == 0) pc <= pc + 1; else pc <= addr_to_jmp;
+                JMP: pc <= addr_to_jmp;
+                default: pc <= pc + 1;
+            endcase
+
+always@(posedge clk)
+    if(stage_counter == 4)
+        case (cop)
+            MTR: RF[addr_r_1_MTR] <= res;
+            RTR: RF[addr_r_1] <= res;
+            SUB, SUM: RF[addr_r_3] <= res;
+            MTRK: RF[addr_r_2] <= res;
+        endcase
+
+always@(posedge clk)
+    if(stage_counter == 4)
+        case (cop)
+            LTM, RTM: mem[res[`hi]] <= res[`lo];
+        endcase
+
+endmodule
+//Сначала ресет перед анчалом симуляции
